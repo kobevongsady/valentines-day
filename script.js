641ce741c5125062b10eacbd03b8833f43eaf26e
kobevongsady <kobevongsady@gmail.com>
2025-02-05 11:19:43 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
-// script.js
-
-// Function to handle button click events
 function selectOption(option) {
-    // Check which option was clicked
     if (option === 'yes') {
-        // Flash rainbow colors
-        flashRainbowColors(function() {
+        // Flash rainbow colors and then display the heart image
+        flashRainbowColors(function () {
             document.getElementById('question').style.display = 'none'; // Hide the question
-            displayCatHeart(); // Display the cat-heart.gif
+            displayMinionHeart(); // Display the heart image
         });
     } else if (option === 'no') {
-        // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
-        // Increase font size of "Yes" button
+        // Change the "No" button text to "You sure?"
+        document.getElementById('no-button').innerText = 'You sure?';
+        // Increase the font size of the "Yes" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
-        var newSize = parseFloat(currentFontSize) * 2; // Increase font size by  * 2px
+        var newSize = parseFloat(currentFontSize) * 2; // Double the font size
         yesButton.style.fontSize = newSize + 'px';
     } else {
-        // If neither "Yes" nor "No" was clicked, show an alert message
+        // Handle unexpected options
         alert('Invalid option!');
     }
 }
 
-// Function to flash rainbow colors and then execute a callback function
 function flashRainbowColors(callback) {
     var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
     var i = 0;
-    var interval = setInterval(function() {
+    var interval = setInterval(function () {
         document.body.style.backgroundColor = colors[i];
         i = (i + 1) % colors.length;
     }, 200); // Change color every 200 milliseconds
-    setTimeout(function() {
+    setTimeout(function () {
         clearInterval(interval);
         document.body.style.backgroundColor = ''; // Reset background color
         if (callback) {
@@ -40,33 +35,30 @@ function flashRainbowColors(callback) {
     }, 2000); // Flash colors for 2 seconds
 }
 
-// Function to display the cat.gif initially
+// Function to display the initial minion image
 function displayMinion() {
-    // Get the container where the image will be displayed
     var imageContainer = document.getElementById('image-container');
     var minionImage = new Image();
-    minionImage.src = 'minion.gif'; 
-    minionImage.alt = 'minion';
-    minionImage.onload = function() {
+    minionImage.src = 'minion.jpg'; // Updated file extension to match the actual file
+    minionImage.alt = 'Minion';
+    minionImage.onload = function () {
         imageContainer.appendChild(minionImage);
     };
 }
 
-// Function to display the cat-heart.gif
-function displayminionHeart() {
-    // Clear existing content in the image container
-    document.getElementById('image-container').innerHTML = '';
-    // Get the container where the image will be displayed
+// Function to display the minion-heart image
+function displayMinionHeart() {
+    document.getElementById('image-container').innerHTML = ''; // Clear existing content
     var imageContainer = document.getElementById('image-container');
     var minionHeartImage = new Image();
-    minionHeartImage.src = 'minionheart.gif'; 
-    // Set alternative text for the image (for accessibility)
+    minionHeartImage.src = 'minionheart.jpg'; // Updated file extension to match the actual file
     minionHeartImage.alt = 'Minion heart';
-    minionHeartImage.onload = function() {
+    minionHeartImage.onload = function () {
         imageContainer.appendChild(minionHeartImage);
-        // Hide the options container
-        document.getElementById('options').style.display = 'none';
+        document.getElementById('options').style.display = 'none'; // Hide the options
     };
 }
 
-displayminionHeart();
+// Initially display the minion image
+displayMinion();
+﻿
